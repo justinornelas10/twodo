bf4b1221becf96d883566edd74cc97cbc8ccd67d
justinornelas10 <jose.ornelas@cceo.com.mx>
2025-03-28 15:02:20 -0600

    feat: :sparkles: Correct checkbox and stepper

--- src/pass_through/index.js
+++ src/pass_through/index.js
@@ -4,7 +4,10 @@ export default {
     },
     datepicker: {
         day: {class: 'hover:!bg-primary-100 !text-black'},
-        panel: {class: '!border-none !w-[40vw]'}
+        panel: {class: '!border-none !w-[40vw]'},
+        pcInputText: {
+            root: {class: '!bg-inherit !border-none !shadow-none !rounded-2xl'}
+        }
     },
     toggleswitch: {
         handle: { class: '!bg-primary-500'}
@@ -13,5 +16,11 @@ export default {
         pcInputText: {
             root: {class: '!bg-inherit !border-none !shadow-none !rounded-2xl'}
         }
+    },
+    stepper: {
+        step: {
+            title: {class: '!text-primary-500'}
+        }
     }
+
 }
